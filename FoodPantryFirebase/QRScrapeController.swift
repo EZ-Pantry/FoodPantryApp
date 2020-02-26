dc56df9c7589a798a7514afafe6cfda50157bb26
ashayp22 <ashayparikh22@gmail.com>
2020-02-25 23:02:57 -0600

    done with inventory firebase, barcode firebase, loading indicator, picker ui

--- FoodPantryFirebase/QRScrapeController.swift
+++ FoodPantryFirebase/QRScrapeController.swift
@@ -44,6 +44,8 @@ class QRScrapeController: UIViewController {
     
     var checkedOut = ""
     
+    var maxQuantity: Int = 0
+    
     override func viewDidAppear(_ animated: Bool) { //https://stackoverflow.com/questions/29257670/alertcontroller-is-not-in-the-window-hierarchy
         super.viewDidAppear(animated)
 
@@ -69,45 +71,84 @@ class QRScrapeController: UIViewController {
             ref = Database.database().reference()
         getData { (title, ingredients, url) in
             DispatchQueue.main.async {
-                
+                print(title)
                 if(title == "no food" || title == "error") {
                     self.errorMessage = ingredients;
                      self.performSegue(withIdentifier: "barcodeError", sender: self)
                 } else {
-                    self.food_title = title;
-                    self.nameLabel.text = title;
-                    self.ingredientsLabel.text = ingredients
-                
-                    var allergies = ["corn", "egg", "fish", "milk", "nut", "soy", "wheat"]
-                
-                    var confirmed = ""
-                
-                    for allergy in allergies {
-                        if title.contains(allergy) {
-                            confirmed += allergy + ","
-                        }  else if(ingredients.contains(allergy)) {
-                            confirmed += allergy + ","
+                    
+                    //check if it is in the database
+                    
+                    self.getFoodDataFromFirebase(callback: {(data, items)-> Void in
+                        
+                        if(items.contains(title)) { //inside data
+                            var index: Int = items.firstIndex(of: title)!
+
+                            let quantity: Int = Int(data[index]["quantity"] as! String) ?? 0
+                            self.maxQuantity = quantity
+                            if(quantity <= 0) {
+                                self.errorMessage = "this food item has ran out";
+                                self.performSegue(withIdentifier: "barcodeError", sender: self)
+                            } else {
+                            
+                            self.food_title = title;
+                            self.nameLabel.text = title;
+                            self.ingredientsLabel.text = data[index]["information"] as! String
+                            
+                            var allergies = ["corn", "egg", "fish", "milk", "nut", "soy", "wheat"]
+                            
+                                var confirmed = ""
+                            
+                                for allergy in allergies {
+                                    if title.contains(allergy) {
+                                        confirmed += allergy + ","
+                                    }  else if(ingredients.contains(allergy)) {
+                                        confirmed += allergy + ","
+                                    }
+                                }
+                            
+                                if confirmed == "" {
+                                    confirmed = "none,"
+                                }
+                            
+                                self.typeLabel.text = confirmed.substring(to: confirmed.count-1);
+                            
+                            
+                                if url != "" {
+                                    self.foodView.load(url: URL(string: url)!);
+                                }
+                            
+                            self.healthyLabel.text = data[index]["healthy"] as! String
+
+                            var text = ""
+                            var str: String = self.checkedOut
+                            
+                            while str.count > 0 {
+                                let food = str.substring(to: str.indexDistance(of: "$")!)
+                                str = str.substring(from: str.indexDistance(of: "$")! + 1)
+                                let quantity = str.substring(to: str.indexDistance(of: ";")!)
+                                text += "Food: " + food + ", Quantity: " + quantity + "\n\n"
+                                str = str.substring(from: str.indexDistance(of: ";")! + 1)
+                            }
+                            
+                            self.currentLabel.text = text
+                            
+                            }
+                        } else {
+                            self.errorMessage = "food item not found in the inventory";
+                            self.performSegue(withIdentifier: "barcodeError", sender: self)
                         }
-                    }
-                
-                    if confirmed == "" {
-                        confirmed = "none,"
-                    }
-                
-                    self.typeLabel.text = confirmed.substring(to: confirmed.count-1);
+                        
+                        
+                        
+                    })
                     
-                    if url != "" {
-                        self.foodView.load(url: URL(string: url)!);
-                    }
+                
                     
-//                    self.dismiss(animated: false)
-//                    print("dismissed")
                 }
             }
         }
-        
-        self.healthyLabel.text = "yes"
-        
+                
         checkoutButton.layer.cornerRadius = 15
         checkoutButton.clipsToBounds = true
            
@@ -118,19 +159,39 @@ class QRScrapeController: UIViewController {
         quantityField.keyboardType = UIKeyboardType.numberPad
         
         
-        var text = ""
-        var str: String = checkedOut
         
-        while str.count > 0 {
-            let food = str.substring(to: str.indexDistance(of: "$")!)
-            str = str.substring(from: str.indexDistance(of: "$")! + 1)
-            let quantity = str.substring(to: str.indexDistance(of: ";")!)
-            text += "Food: " + food + ", Quantity: " + quantity + "\n\n"
-            str = str.substring(from: str.indexDistance(of: ";")! + 1)
-        }
+    }
+    
+    func getFoodDataFromFirebase(callback: @escaping (_ data: [[String: Any]], _ names: [String])->Void) {
+        self.ref = Database.database().reference()
         
-        currentLabel.text = text
         
+        self.ref.child("Conant High School").child("Inventory").child("Food Items").observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            var tempData : [[String: Any]] = []
+            var tempNames: [String] = []
+            var c: Int = 0
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                let key = snap.key
+                let value: [String: Any] = snap.value as! [String : Any]
+                
+                let name = value["Name"] as? String ?? ""
+                let url = value["URL"] as? String ?? ""
+                let checked = value["Checked Out"] as? String ?? ""
+                let healthy = value["Healthy"] as? String ?? ""
+                let quantity = value["Quantity"] as? String ?? ""
+                let type = value["Type"] as? String ?? ""
+                let info = value["Information"] as? String ?? ""
+                let id = String(c)
+                
+                tempData.append(["name": name, "quantity": quantity, "amountCheckedOut": checked, "information": info, "healthy": healthy, "image": url, "id": id])
+                tempNames.append(name)
+                c += 1
+            }
+            
+             callback(tempData, tempNames)
+        })
     }
 
     
@@ -177,13 +238,86 @@ class QRScrapeController: UIViewController {
        }
     
     
-    //check if the food item exists
-    //then subtrac
+    @IBAction func changedQuantity(_ sender: Any) {
+        
+        var currentQuantity: Int = Int(quantityField.text as! String)!
+        
+        if currentQuantity <= 0 {
+            quantityField.text = "1"
+        } else if currentQuantity > maxQuantity {
+            quantityField.text = String(maxQuantity)
+        }
+        
+    }
+    
     
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         checkedOut += food_title + "$" + quantityField.text! + ";"
+        
+        //check to see if there are any food titles that are duplictated
+        
+        var items: [String] = []
+        var quantities: [Int] = []
+        
+        var str: String = checkedOut
+        
+        print(checkedOut)
+        
+        while str.count > 0 {
+            let food = str.substring(to: str.indexDistance(of: "$")!)
+            print(food)
+            items.append(food)
+            str = str.substring(from: str.indexDistance(of: "$")! + 1)
+            let quantity = str.substring(to: str.indexDistance(of: ";")!)
+            print(quantity)
+            quantities.append(Int(quantity) ?? 0)
+            str = str.substring(from: str.indexDistance(of: ";")! + 1)
 
+        }
+        
+        var merged : [[String: Any]] = []
+        
+        while items.count > 0 {
+            let food = items[0]
+            var quantity: Int = 0
+            
+            var matched: [Int] = []
+            
+            for i in 0..<items.count {
+                if items[i] == food {
+                    quantity += quantities[i]
+                    matched.append(i)
+                }
+            }
+            
+            print(matched)
+            print(items)
+            
+            for i in 0..<matched.count{
+                items.remove(at: matched[i])
+                quantities.remove(at: matched[i])
+                matched = matched.map{ $0 - 1 } //subtracts 1 from every match since the array decreased in size
+            }
+            
+            merged.append(["name": food, "quantity": quantity])
+            
+        }
+        
+//        print(checkedOut)
+//        print("changed")
+        checkedOut = ""
+        
+        for val in merged {
+            checkedOut += (val["name"] as! String) + "$" + String(val["quantity"] as! Int) + ";"
+            print(val["name"] as! String)
+            print(String(val["quantity"] as! Int))
+        }
+        
+//        print(checkedOut)
+        
+        
+        //continue with segue
         if segue.identifier == "barcodeError"{
             
             let destinationVC = segue.destination as? QRCodeViewController
@@ -192,6 +326,11 @@ class QRScrapeController: UIViewController {
         } else if(segue.identifier == "addMore") {
             let destinationVC = segue.destination as? QRCodeViewController
             destinationVC?.checkedOut = checkedOut
+        } else if(segue.identifier == "checkOut") {
+            let destinationVC = segue.destination as? checkoutViewController
+            print("checking out")
+            destinationVC?.foodItems = checkedOut
+            print("set")
         }
     }
 
