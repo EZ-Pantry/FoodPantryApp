0faae7d4338373e155e8940a150a9818c5b71ae9
ashayp22 <ashayparikh22@gmail.com>
2020-03-08 00:16:18 -0600

    fresh new update - redesigned storyboard, signup, fixed some errors, combined ashay and rayaan

--- FoodPantryFirebase/addItemViewController.swift
+++ FoodPantryFirebase/addItemViewController.swift
@@ -1,10 +1,5 @@
-//
-//  addItemViewController.swift
-//  FoodPantryFirebase
-//
-//  Created by Ashay Parikh on 3/1/20.
-//  Copyright © 2020 Rayaan Siddiqi. All rights reserved.
-//
+//  Copyright © 2020 Ashay Parikh, Rayaan Siddiqi. All rights reserved.
+
 
 import Foundation
 import UIKit
