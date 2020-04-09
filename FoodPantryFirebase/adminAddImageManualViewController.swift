70c08d361890eb409b15e0af819d884db0ce402c
RaySCS <rayaan.siddiqi@gmail.com>
2020-04-09 18:40:17 -0500

    Added error message if bing is down

--- FoodPantryFirebase/adminAddImageManualViewController.swift
+++ FoodPantryFirebase/adminAddImageManualViewController.swift
@@ -53,6 +53,7 @@ class adminAddImageManualViewController: UIViewController, UITextFieldDelegate {
         print(foodItemEnteringName)
 //        https://www.google.com/search?tbm=isch&as_q=vienna&tbs=isch
 //        https://www.google.com/search?tbm=isch&as_q=vienna&tbs=isz:lt,islt:4mp,sur:fmc
+        
         let fullFoodNameArr = foodItemEnteringName.components(separatedBy: " ")
         
         fullWordWithPlus = foodItemEnteringName
@@ -95,47 +96,55 @@ class adminAddImageManualViewController: UIViewController, UITextFieldDelegate {
             }
             else{
                 let htmlContent = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)//the HTML
-                print(htmlContent)
-                let stringOfHTMLContent: String = htmlContent as! String
-                print("other one below")
-                print(stringOfHTMLContent)
-                print(stringOfHTMLContent.indices(of: "src="))
-                self.indiciesOfSRC.append(contentsOf: stringOfHTMLContent.indices(of: "src="))
-                print(self.indiciesOfSRC)//provides begining indicies
-                
-//                http://t1.gstatic.com/images?q=tbn:ANd9GcReGv2aOtZhU2V1Wf8GjWSkKdp7s3bGmHpl_iPVkeVXYXr2tHVJcVu98Tk
-                
-                print("printing other below")
-                for x in 0..<self.indiciesOfSRC.count{
-//                    print(stringOfHTMLContent.between(self.indiciesOfSRC[x], self.indiciesOfSRC[x] + 99))
-//get first ss to last one
-//                    var firstIndex = stringOfHTMLContent.substring(from: self.indiciesOfSRC[x]);
-//                    print(firstIndex)
-                    
-                    print("other newest one")
-                    let currentRange = stringOfHTMLContent.substring(with: self.indiciesOfSRC[x]..<self.indiciesOfSRC[x] + 104)//do everything with this!
-                    print(currentRange)
-                    print(currentRange.substring(from: 5))
-                    self.imageSRCData.append(currentRange.substring(from: 5))//all the src gathered, now get https ones
-                    
-//                    let range = text.substring(0..<3)
-//                    print("")
-//                    var lastIndexTo = stringOfHTMLContent.substring(from: self.indiciesOfSRC[x] + 99)
-//                    print(lastIndexTo)
-//                    print("")
-//                    print(stringOfHTMLContent.between(firstIndex, lastIndexTo))
+                if(htmlContent == nil){
+                    let alert = UIAlertController(title: "This Feature is currently unavailable", message: "Please try again later!", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil);
                 }
-                
-                for y in 0..<self.imageSRCData.count{
-                    if(self.imageSRCData[y].contains("http")){
-                        print("contains http")
-                        self.modifiedSRCData.append(self.imageSRCData[y])//getting the actual images only with good link
-                    }
+                else{
+                     print(htmlContent)
+                                    let stringOfHTMLContent: String = htmlContent as! String
+                                    print("other one below")
+                                    print(stringOfHTMLContent)
+                                    print(stringOfHTMLContent.indices(of: "src="))
+                                    self.indiciesOfSRC.append(contentsOf: stringOfHTMLContent.indices(of: "src="))
+                                    print(self.indiciesOfSRC)//provides begining indicies
+                                    
+                    //                http://t1.gstatic.com/images?q=tbn:ANd9GcReGv2aOtZhU2V1Wf8GjWSkKdp7s3bGmHpl_iPVkeVXYXr2tHVJcVu98Tk
+                                    
+                                    print("printing other below")
+                                    for x in 0..<self.indiciesOfSRC.count{
+                    //                    print(stringOfHTMLContent.between(self.indiciesOfSRC[x], self.indiciesOfSRC[x] + 99))
+                    //get first ss to last one
+                    //                    var firstIndex = stringOfHTMLContent.substring(from: self.indiciesOfSRC[x]);
+                    //                    print(firstIndex)
+                                        
+                                        print("other newest one")
+                                        let currentRange = stringOfHTMLContent.substring(with: self.indiciesOfSRC[x]..<self.indiciesOfSRC[x] + 104)//do everything with this!
+                                        print(currentRange)
+                                        print(currentRange.substring(from: 5))
+                                        self.imageSRCData.append(currentRange.substring(from: 5))//all the src gathered, now get https ones
+                                        
+                    //                    let range = text.substring(0..<3)
+                    //                    print("")
+                    //                    var lastIndexTo = stringOfHTMLContent.substring(from: self.indiciesOfSRC[x] + 99)
+                    //                    print(lastIndexTo)
+                    //                    print("")
+                    //                    print(stringOfHTMLContent.between(firstIndex, lastIndexTo))
+                                    }
+                                    
+                                    for y in 0..<self.imageSRCData.count{
+                                        if(self.imageSRCData[y].contains("http")){
+                                            print("contains http")
+                                            self.modifiedSRCData.append(self.imageSRCData[y])//getting the actual images only with good link
+                                        }
+                                    }
+                                    self.loadFirstImage();
+                                    //next 99 characters
+                                    
+                                }
                 }
-                self.loadFirstImage();
-                //next 99 characters
-                
-            }
+               
         
         }
         task.resume()
