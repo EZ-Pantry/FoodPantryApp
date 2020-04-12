5dafcf6e46dab5c4d7e34b8d080f81a3a4475880
ashayp22 <ashayparikh22@gmail.com>
2020-04-12 11:06:31 -0500

    tweaked some stuff, still has bugs

--- FoodPantryFirebase/logInViewController.swift
+++ FoodPantryFirebase/logInViewController.swift
@@ -72,72 +72,7 @@ class logInViewController: UIViewController, UITextFieldDelegate {
                         var emailaddress = UserDefaults.standard.object(forKey:"Latest Email") as! String
                         var password = UserDefaults.standard.object(forKey:"Latest Password") as! String
                         if(canUseBiometrics == "Good"){
-                            Auth.auth().signIn(withEmail: emailaddress, password: password){ user, error in
-                                
-                                if(error != nil) {
-                                    //else show error message
-                                    let alert = UIAlertController(title: "Error Logging In", message: "Please try again!", preferredStyle: .alert)
-                                    alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
-                                    self.present(alert, animated: true, completion: nil);
-                                } else {
-                                
-                                        let user = Auth.auth().currentUser
-
-                                    
-                                        if(user!.isEmailVerified) {
-                                                //check if admin allowed
-                                                let user = Auth.auth().currentUser
-                                                self.ref.child("All Users").child(user!.uid).observeSingleEvent(of: .value, with: { (snapshot) in
-                                                // Get user value
-                                                let value = snapshot.value as? NSDictionary
-                                                 let status = value?["Account Status"] as? String ?? "" //load in the admin code
-                                                  
-                                                    if(status != "0") {
-                                                        
-                                                        if(status == "2") {
-                                                            UserDefaults.standard.set("Bad", forKey: "Times Unlocked")
-                                                            let alert = UIAlertController(title: "Your Account has Been Suspended", message: "The admin has suspended this account.", preferredStyle: .alert)
-                                                                                                 
-                                                            alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: { (action: UIAlertAction!) in
-                                                                
-                                                                UserDefaults.standard.set("Good", forKey: "Times Unlocked")
-                                                                UserDefaults.standard.set(emailaddress, forKey: "Latest Email")
-                                                                UserDefaults.standard.set(password, forKey: "Latest Password")
-                                                                self.performSegue(withIdentifier: "toHome", sender: self)//performs segue to the home screen to show user data with map
-                                                            }))
-                                                            self.present(alert, animated: true, completion: nil);
-                                                            
-                                                            
-                                                        } else {
-                                                            UserDefaults.standard.set("Good", forKey: "Times Unlocked")
-                                                            UserDefaults.standard.set(emailaddress, forKey: "Latest Email")
-                                                            UserDefaults.standard.set(password, forKey: "Latest Password")
-                                                            self.performSegue(withIdentifier: "toHome", sender: self)//performs segue to the home screen to show user data with map
-                                                        }
-                                                        
-                                                        
-                                                    } else {
-                                                        UserDefaults.standard.set("Bad", forKey: "Times Unlocked")
-                                                        let alert = UIAlertController(title: "Account Not Approved", message: "This account has not been approved by the admin", preferredStyle: .alert)
-                                                        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
-                                                        self.present(alert, animated: true, completion: nil);
-                                                    }
-                                                    
-                                                // ...
-                                                }) { (error) in
-                                                    RequestError().showError()
-                                                    print(error.localizedDescription)
-                                                }
-                                        } else {
-                                            let alert = UIAlertController(title: "Email Not Verified", message: "Please check your inbox/spam folder and make sure you have verified your email!", preferredStyle: .alert)
-                                            alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
-                                            self.present(alert, animated: true, completion: nil);
-                                        }
-                                    
-                                
-                                }
-
-                            }
+                            self.signInUser(email: emailaddress, password: password)
                         }
                         else{
                             print("not")
@@ -162,42 +97,18 @@ class logInViewController: UIViewController, UITextFieldDelegate {
             print(error)
         }
     }
-//
-//    func authenticateWithBiometrics(){
-//        let context = LAContext()
-//        var error: NSError?
-//
-//        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-//            let reason = "Identify yourself!"
-//
-//            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
-//                [weak self] success, authenticationError in
-//
-//                DispatchQueue.main.async {
-//                    if success {
-//                        let ac = UIAlertController(title: "Authentication Good", message: "You could  be verified; please try again.", preferredStyle: .alert)
-//                        ac.addAction(UIAlertAction(title: "OK", style: .default))
-//                        self!.present(ac, animated: true)
-//                    } else {
-//                        let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
-//                        ac.addAction(UIAlertAction(title: "OK", style: .default))
-//                        self!.present(ac, animated: true)
-//                    }
-//                }
-//            }
-//        } else {
-//            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
-//            ac.addAction(UIAlertAction(title: "OK", style: .default))
-//            self.present(ac, animated: true)
-//        }
-//    }
-//
+
     @IBAction func handleContinue(_ sender: UIButton) {
         guard let emailaddress = emailAddressTextField.text else { return }
         guard let password = passwordTextField.text else { return }
         
         print(password)
-        Auth.auth().signIn(withEmail: emailaddress, password: password){ user, error in
+        signInUser(email: emailaddress, password: password)
+        
+    }
+    
+    func signInUser(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password){ user, error in
             
             if(error != nil) {
                 //else show error message
@@ -226,7 +137,7 @@ class logInViewController: UIViewController, UITextFieldDelegate {
                                         alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: { (action: UIAlertAction!) in
                                             
                                             UserDefaults.standard.set("Good", forKey: "Times Unlocked")
-                                            UserDefaults.standard.set(emailaddress, forKey: "Latest Email")
+                                            UserDefaults.standard.set(email, forKey: "Latest Email")
                                             UserDefaults.standard.set(password, forKey: "Latest Password")
                                             self.performSegue(withIdentifier: "toHome", sender: self)//performs segue to the home screen to show user data with map
                                         }))
@@ -235,7 +146,7 @@ class logInViewController: UIViewController, UITextFieldDelegate {
                                         
                                     } else {
                                         UserDefaults.standard.set("Good", forKey: "Times Unlocked")
-                                        UserDefaults.standard.set(emailaddress, forKey: "Latest Email")
+                                        UserDefaults.standard.set(email, forKey: "Latest Email")
                                         UserDefaults.standard.set(password, forKey: "Latest Password")
                                         self.performSegue(withIdentifier: "toHome", sender: self)//performs segue to the home screen to show user data with map
                                     }
@@ -263,11 +174,6 @@ class logInViewController: UIViewController, UITextFieldDelegate {
             }
 
         }
-        
-    }
-    
-    func checkWhetherPasswordNeedsUpdate(){
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
